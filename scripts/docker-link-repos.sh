1242c19a63297741f1e40f1e47bd008335433dc8
你的名字 <avatar.xu@gmail.com>
2022-11-16 16:07:07 +0800

    Develop dockerfile

--- scripts/docker-link-repos.sh
+++ scripts/docker-link-repos.sh
@@ -2,23 +2,6 @@
 
 set -ex
 
-# Automatically link to develop if we're building develop, but only if the caller
-# hasn't asked us to build something else
-BRANCH=$(git rev-parse --abbrev-ref HEAD)
-if [[ $USE_CUSTOM_SDKS == false ]] && [[ $BRANCH == 'develop' ]]
-then
-    echo "using develop dependencies for react-sdk and js-sdk"
-    USE_CUSTOM_SDKS=true
-    JS_SDK_BRANCH='develop'
-    REACT_SDK_BRANCH='develop'
-fi
-
-if [[ $USE_CUSTOM_SDKS == false ]]
-then
-    echo "skipping react-sdk and js-sdk installs: USE_CUSTOM_SDKS is false"
-    exit 0
-fi
-
 echo "Linking js-sdk"
 git clone --depth 1 --branch $JS_SDK_BRANCH "$JS_SDK_REPO" js-sdk
 cd js-sdk
